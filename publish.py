34d0c1e7dd8bb02b17b23cdad89856da1ebe6f93
Paul Seyfert <pseyfert@cern.ch>
2018-01-11 14:54:42 +0100

    publish.py code polishing (2to3)

--- publish.py
+++ publish.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 # push slides for a talk to cern gitlab
-# 
+#
 # TODO: document use case
 # TODO: error handling
 # TODO: more proper usage of python (e.g. git module instad of check_output)
@@ -11,53 +11,70 @@
 
 import os
 import sys
+import json
 from subprocess import check_output
 
 WorldPublic = True
 TrivialName = os.path.basename(os.getcwd())
 
 if WorldPublic:
-    out = check_output(["curl","--header","PRIVATE-TOKEN: "+os.environ["GITLABTOKEN"],"-X","POST","https://gitlab.cern.ch/api/v3/projects?name="+TrivialName+"&visibility_level=20"])
-    check_output(["mv","LICENSE.pub.md","LICENSE.md"])
-    check_output(["rm","LICENSE.int.md"])
+    out = check_output(["curl",
+                        "--header", "PRIVATE-TOKEN: "+os.environ["GITLABTOKEN"],
+                        "-X", "POST",
+                        "https://gitlab.cern.ch/api/v3/projects?name="+TrivialName+"&visibility_level=20"
+                        ])
+    # these fail upon try-again due to failure
+    check_output(["mv", "LICENSE.pub.md", "LICENSE.md"])
+    check_output(["rm", "LICENSE.int.md"])
 else:
-    out = check_output(["curl","--header","PRIVATE-TOKEN: "+os.environ["GITLABTOKEN"],"-X","POST","https://gitlab.cern.ch/api/v3/projects?name="+TrivialName+"&visibility_level=0"])
-    check_output(["mv","LICENSE.int.md","LICENSE.md"])
-    check_output(["rm","LICENSE.pub.md"])
+    out = check_output(["curl",
+                        "--header", "PRIVATE-TOKEN: "+os.environ["GITLABTOKEN"],
+                        "-X", "POST",
+                        "https://gitlab.cern.ch/api/v3/projects?name="+TrivialName+"&visibility_level=0"
+                        ])
+    # these fail upon try-again due to failure
+    check_output(["mv", "LICENSE.int.md", "LICENSE.md"])
+    check_output(["rm", "LICENSE.pub.md"])
     print("TODO share with LHCb")
 
-check_output(["git","rm","logo.png"])
+check_output(["git", "rm", "logo.png"])
 
-import json
+# json call like this not ready for python3
 repo_conf = json.loads(out)
 try:
     repo_conf["name"]
 except:
-    print "Oh help us"
+    # likely repo already exists (try-again? name collision?)
+    print("Oh help us")
     sys.exit(1)
 
 try:
     import re
-    #check_output(["git","remote","add",TrivialName,re.sub("7999","8443",re.sub('ssh://git','https://:',repo_conf["ssh_url_to_repo"]))])
-    check_output(["git","remote","add","gitlab",re.sub("7999","8443",re.sub('ssh://git','https://:',repo_conf["ssh_url_to_repo"]))])
+    # check_output(["git","remote","add",TrivialName,re.sub("7999","8443",re.sub('ssh://git','https://:',repo_conf["ssh_url_to_repo"]))])
+    check_output(["git", "remote", "add",
+                  "gitlab",
+                  re.sub("7999", "8443", re.sub('ssh://git', 'https://:', repo_conf["ssh_url_to_repo"]))
+                  ])
 except:
     print("couldn't add remote")
     print(json.dumps(repo_conf, sort_keys=True, indent=2, separators=(',', ': ')))
 
-#check_output(["git","subtree","split","--prefix="+os.path.basename(DirName),"-b",BranchName])
+# check_output(["git","subtree","split","--prefix="+os.path.basename(DirName),"-b",BranchName])
 try:
-    pushout = check_output(["git","push","gitlab","master:master"])
-except: 
-    print "push did ", pushout
+    # check that we are on the master branch
+    pushout = check_output(["git", "push", "gitlab", "master:master"])
+except:
+    # pushout unknown ...
+    print("push did ", pushout)
 
 try:
-    qrgen = check_output(["qrencode","-o","QR.png",repo_conf['web_url']])
+    qrgen = check_output(["qrencode", "-o", "QR.png", repo_conf['web_url']])
 except:
-    print "QR code generation did ", qrgen
+    print("QR code generation did ", qrgen)
 try:
-    convert = check_output(["convert","QR.png","-flatten","QR2.png"])
+    convert = check_output(["convert", "QR.png", "-flatten", "QR2.png"])
 except:
-    print "alpha channel removal did ", convert
+    print("alpha channel removal did ", convert)
 print(" ========================================= ")
 print(" =========   BIG FAT WARNING   =========== ")
 print(" ========================================= ")
